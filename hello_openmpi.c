19d82872598e4f8a2f928a63c85aec3f7b935c79
GoelBiju <goelmolel@hotmail.com>
2019-09-12 14:15:00 +0100

    Altered hello_openmpi.c

--- hello_openmpi.c
+++ hello_openmpi.c
@@ -1,19 +1,28 @@
+/*
+    Hello world and loading openmpi
+*/
 
-#include "mpi.h"
+#include <mpi.h>
 #include <stdio.h>
 
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
-    int rank, size;
-    
-    MPI_Init(&argc, &argv);
+        MPI_Init(NULL, NULL);
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+        int world_size;
+        MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    printf("Hello World. I am %d\n", rank, size);
+        int world_rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    MPI_Finalize();
-    return 0;
+        char processor_name[MPI_MAX_PROCESSOR_NAME];
+        int name_len;
+        MPI_Get_processor_name(processor_name, &name_len);
+
+        printf("Hello world from processor %s, rank %d out of %d processors.\n", processor_name, world_rank, world_size);
+
+        MPI_Finalize();
+
+        return 0;
 }
